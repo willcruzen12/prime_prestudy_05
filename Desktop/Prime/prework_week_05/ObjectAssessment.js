b78278252133fdde055e12e756581384033baeef
Admin <admin@Admins-MacBook-Pro.local>
2016-07-17 20:11:22 -0500

    first commit

---
+++ Desktop/Prime/prework_week_05/ObjectAssessment.js
@@ -0,0 +1,11 @@
+var avengersMovie = {
+	name : "Avengers",
+	runtime : 143 + " mins",
+	releaseYear : 2012,
+	director : "Joss Whedon"
+};
+
+function message() {
+	console.log(avengersMovie); 
+}
+message();
